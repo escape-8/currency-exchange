3cfe1b994dcd873f69c6209dc736b708c513f0cb
Mikhail Tsepelev <cplvmhl@gmail.com>
2024-03-28 18:56:38 +0300

    feat: implement TrailingSlashMiddleware for url

---
+++ middleware/TrailingSlashMiddleware.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Middleware;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use GuzzleHttp\Psr7\Response;
+
+class TrailingSlashMiddleware
+{
+    public function __invoke(Request $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $uri = $request->getUri();
+        $path = $uri->getPath();
+
+        if ($path !== '/' && str_ends_with($path, '/')) {
+            $path = rtrim($path, '/');
+            $uri = $uri->withPath($path);
+
+            if ($request->getMethod() == 'GET') {
+                $response = new Response();
+                return $response
+                    ->withStatus(301)
+                    ->withHeader('Location', (string)$uri);
+            } else {
+                $request = $request->withUri($uri);
+            }
+        }
+
+        return $handler->handle($request);
+    }
+}
--- public/index.php
+++ public/index.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use Middleware\TrailingSlashMiddleware;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
@@ -9,7 +10,7 @@ use Slim\Factory\AppFactory;
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
-$DbConnection = new DatabaseConnection();
+$app->add(new TrailingSlashMiddleware());
 
 $app->get('/', function (Request $request, Response $response, $args) {
     $response->getBody()->write("Hello world!");
