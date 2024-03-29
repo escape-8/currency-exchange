c1fe3410683c027d7fd74db3e3973304747ec4f2
Mikhail Tsepelev <cplvmhl@gmail.com>
2024-03-29 13:10:44 +0300

    refactor: change props to readonly for immutable

--- src/DTO/CurrencyDTO.php
+++ src/DTO/CurrencyDTO.php
@@ -4,9 +4,24 @@ namespace App\DTO;
 
 class CurrencyDTO
 {
-    public int $id;
-    public string $code;
-    public string $name;
-    public string $sign;
+    public readonly int $id;
+    public readonly string $code;
+    public readonly string $name;
+    public readonly string $sign;
+
+    /**
+     * @param int $id
+     * @param string $code
+     * @param string $name
+     * @param string $sign
+     */
+    public function __construct(int $id, string $code, string $name, string $sign)
+    {
+        $this->id = $id;
+        $this->code = $code;
+        $this->name = $name;
+        $this->sign = $sign;
+    }
+
 
 }
