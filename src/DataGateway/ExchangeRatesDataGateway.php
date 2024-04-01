fa14d273876d2a507ba820c477139fe2fb7cc389
Mikhail Tsepelev <cplvmhl@gmail.com>
2024-04-01 12:11:45 +0300

    feat: implement getAllCurrencies method in ExchangeRatesDataGateway

--- src/DataGateway/ExchangeRatesDataGateway.php
+++ src/DataGateway/ExchangeRatesDataGateway.php
@@ -4,14 +4,32 @@ namespace App\DataGateway;
 
 class ExchangeRatesDataGateway extends DataGateway
 {
-    private PDO $dataBase;
-
-    /**
-     * @param PDO $dbConnection
-     */
-    public function __construct(PDO $dbConnection)
+    public function getAllCurrencies(): array
     {
-        $this->dataBase = $dbConnection;
-    }
+        $sql = "SELECT 
+                    exrates.id,
+                    base_target.id AS base_currency_id,
+                    base_target.code AS base_currency_code,
+                    base_target.full_name AS base_currency_name,
+                    base_target.sign AS base_currency_sign,
+                    cur_target.id AS target_currency_id,
+                    cur_target.code AS target_currency_code,
+                    cur_target.full_name AS target_currency_name,
+                    cur_target.sign AS target_currency_sign,
+                    exrates.rate
+                FROM exchange_rates AS exrates
+                JOIN currencies AS cur_target
+                ON target_currency_id=cur_target.id
+                JOIN currencies AS base_target
+                ON base_currency_id=base_target.id;";
 
+        $statement = $this->dataBase->query($sql);
+        $result = $statement->fetchAll();
+
+        if (!$result) {
+            return [];
+        }
+
+        return $result;
+    }
 }
