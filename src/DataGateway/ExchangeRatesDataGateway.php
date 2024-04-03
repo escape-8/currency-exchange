e59c8dc0001a8a8bd3fdb14ce588fe0a4475457a
Mikhail Tsepelev <cplvmhl@gmail.com>
2024-04-03 09:12:54 +0300

    feat: implement getExchangeRatesByIds method

--- src/DataGateway/ExchangeRatesDataGateway.php
+++ src/DataGateway/ExchangeRatesDataGateway.php
@@ -67,4 +67,38 @@ class ExchangeRatesDataGateway extends DataGateway
 
         return $result;
     }
+
+    /**
+     * @throws DatabaseNotFoundException
+     */
+    public function getExchangeRateByIds(int $baseCurrencyId, int $targetCurrencyId): array
+{
+    $sql = "SELECT 
+                exrates.id,
+                cur_base.id AS base_currency_id,
+                cur_base.code AS base_currency_code,
+                cur_base.full_name AS base_currency_name,
+                cur_base.sign AS base_currency_sign,
+                cur_target.id AS target_currency_id,
+                cur_target.code AS target_currency_code,
+                cur_target.full_name AS target_currency_name,
+                cur_target.sign AS target_currency_sign,
+                exrates.rate
+            FROM exchange_rates AS exrates
+            JOIN currencies AS cur_base
+            ON cur_base.id=exrates.base_currency_id
+            JOIN currencies AS cur_target
+            ON cur_target.id=exrates.target_currency_id
+            WHERE cur_base.id = :baseCurrencyId AND cur_target.id= :targetCurrencyId";
+    $statement = $this->dataBase->prepare($sql);
+    $statement->execute(['baseCurrencyId' => $baseCurrencyId, 'targetCurrencyId' => $targetCurrencyId]);
+    $result = $statement->fetch();
+
+    if (!$result) {
+        throw new DatabaseNotFoundException('Exchange rate for pair not found');
+    }
+
+    return $result;
+}
+
 }
