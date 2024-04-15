f3db5cfbcf5eb5d32d7d993becb6dba956937492
Mikhail Tsepelev <cplvmhl@gmail.com>
2024-04-15 14:18:24 +0300

    refactor: swap Exception handler middleware

--- config/middleware.php
+++ config/middleware.php
@@ -6,12 +6,12 @@ use App\Middleware;
 use Slim\App;
 
 return static function (App $app) {
-    $app->add((new Middleware\ExceptionHandler()));
     $app->add(new Middleware\PDOExceptionMiddleware());
     $app->add(new Middleware\DatabaseNotFoundExceptionHandler());
     $app->add(new Middleware\ValidationExceptionHandler());
     $app->add(new Middleware\URLCurrencyCodesMiddleware());
     $app->add(new Middleware\CORSResponseMiddleware());
+    $app->add((new Middleware\ExceptionHandler()));
     $app->addBodyParsingMiddleware();
     $app->addRoutingMiddleware();
     $app->addErrorMiddleware(false,true,true);
